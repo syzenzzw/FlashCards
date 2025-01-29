da8457fc297c3ca59fd75662c8d164e2bb7de290
syzenzzw <oisyzenx@gmail.com>
2025-01-29 17:26:27 -0300

    first commit

---
+++ src/components/component.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useQuery } from 'react-query'
+import { useEffect, useState } from "react";
+
+function ComponentTestReactQuery(){
+    const {data, isLoading, error} = useQuery('getCards', () => {
+        return axios
+        .get("https://localhost:7091/api/v1/GetAllCards?pageIndex=1&pageSize=20")
+        .then((response) => response.data);
+
+    },{
+        retry: 5,
+        refetchOnWindowFocus: true,
+
+    });
+
+    if (isLoading) {    
+        return <div className="carregando">
+            Carregando...
+        </div>
+    } 
+
+    if (error) {
+        return <div className="loading">
+            algo deu errado
+        </div>
+    }
+
+    return(
+        <div>
+            <h1>Cards: </h1>
+            {data.cards.map(card => (
+                <div key={card.id} className="lis">   
+                {card.content}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default ComponentTestReactQuery;   
