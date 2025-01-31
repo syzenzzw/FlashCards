f191917b1d7ea2976944c19a3e7cee09b93ff617
syzenzzw <oisyzenx@gmail.com>
2025-01-31 15:03:14 -0300

    add function for change the revised

---
+++ src/components/createcard.tsx
@@ -0,0 +1,5 @@
+function CreateCard(){
+
+}
+
+export default CreateCard;
--- src/components/flashsmenu.tsx
+++ src/components/flashsmenu.tsx
@@ -1,9 +1,13 @@
 import axios from "axios"
 import { useQuery } from 'react-query'
 import '../styles/flashmenu.css'
-
+import { useState } from "react";
+import '../styles/card.css'
+import polegar from './imgs/polegar.png'
 
 function FlashMenu(){ 
+    const [cards, setCards] = useState(null);
+
     const {data, isLoading, error} = useQuery('getAllCards', () => {
         return axios
         .get('https://localhost:7091/api/v1/GetAllCards?pageIndex=1&pageSize=20')
@@ -23,16 +27,31 @@ function FlashMenu(){
         </div>
     }
 
-    async function responseGetById(id: number) {
+     async function responseGetById(id: number) {
         const response = await axios.get(`https://localhost:7091/api/v1/PegarPelaId${id}`)
-        const data = await response.data;
-        console.log(data);
-        return(
-            <div>
-                
-            </div>
-        )        
+        const dataResponse = await response.data;
+        setCards(dataResponse);
+        console.log(dataResponse);           
     }
+
+    async function changeRevised(id: number) {
+        const responseRevised = await fetch(`https://localhost:7091/api/v1/ChangeRevised${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "revised": true
+            }),
+        })
+
+        if (responseRevised.status === 200) {
+            console.log(responseRevised)
+            responseGetById(id);
+        }
+    }
+
+    
 
     return(
         <div className="divtaskbar">
@@ -51,6 +70,39 @@ function FlashMenu(){
                 </div>
             ))}
         </div>
+            {cards && (
+                <div className="containerCard">
+                       <div className="contentCard">
+                            <label htmlFor="polegar">Revisado? </label>
+
+                            <label
+                            onClick={() => changeRevised(cards.id)} 
+                            className="polegar" 
+                            id="polegar"
+                            >
+                            <img width={20} src={polegar} />
+                            </label>
+
+                            <p className="content">{cards.content}</p>
+
+                            <br /><br />
+
+                            <small className="createdOnCard">Criado em: {cards.formattedCreatedOn}</small>
+
+                            <br /><br />
+                            
+                            <small className="urgency">Urgência: {cards.urgency}</small>
+                            
+                            <br /><br />
+
+                            <small className="Isrevised" style={
+                                {
+                                    color: cards.revised === 'Sim' ? '#00FF00' : 'inherit',
+                                }
+                            }>Revisado: {cards.revised}</small>
+                       </div>   
+                </div>
+            )}
         </div>
     );
 }
